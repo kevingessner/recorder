b69ed76654462d75535b34e281d6edd3f7dacd01
Kevin Gessner <kevin@kevingessner.com>
2022-06-16 10:55:56 -0400

    store the recording

--- recorder.js
+++ recorder.js
@@ -1,16 +1,38 @@
-import { get, set } from 'https://cdn.jsdelivr.net/npm/idb-keyval@6/+esm';
+import { get as getFromStorage, set as setToStorage } from 'https://cdn.jsdelivr.net/npm/idb-keyval@6/+esm';
 
 export default function init(elBtnRecord, elBtnPlay) {
+  const StorageKey = 'recorder.recording';
+
   const state = {
     hasRecording: false,
   };
   Object.seal(state);
-  function updateUIFromState(_) {
+  function updateUIFromState() {
     elBtnPlay.disabled = elBtnPlay.ariaDisabled = !state.hasRecording;
   }
 
-  async function saveRecordingData(blob) {
+  function logError(err) {
+    console.log(err);
+    // TODO display to the user
+  }
+
+  function handleErrors(fn) {
+    return function withErrorHandling() {
+      try {
+        const res = fn(...arguments);
+        if (res && res.catch) {
+          res.catch(logError);
+        }
+        return res;
+      } catch (e) {
+        logError(e);
+      }
+    };
+  }
+
+  async function playFromStorage() {
     const audioCtx = new AudioContext();
+    const blob = await getFromStorage(StorageKey);
     const buffer = await blob.arrayBuffer();
     const decoded = await audioCtx.decodeAudioData(buffer);
 
@@ -21,12 +43,15 @@ export default function init(elBtnRecord, elBtnPlay) {
     source.start();
   }
 
+  async function saveRecordingData(blob) {
+    setToStorage(StorageKey, blob);
+    updateUIFromState(state.hasRecording = true);
+    await playFromStorage();
+  }
+
   function recordStream(stream) {
     const recorder = new MediaRecorder(stream);
-    recorder.addEventListener('dataavailable', (event) => {
-      updateUIFromState(state.hasRecording = true);
-      saveRecordingData(event.data);
-    });
+    recorder.addEventListener('dataavailable', handleErrors((event) => saveRecordingData(event.data)));
     setTimeout(() => recorder.stop(), 2000);
     recorder.start();
   }
@@ -34,10 +59,11 @@ export default function init(elBtnRecord, elBtnPlay) {
   function startRecording() {
     navigator.mediaDevices.getUserMedia({ audio: true })
       .then((stream) => recordStream(stream))
-      .catch((err) => { console.log(`The following gUM error occured: ${err}`); });
+      .catch(logError);
   }
 
-  elBtnRecord.addEventListener('click', (event) => startRecording());
+  elBtnRecord.addEventListener('click', handleErrors((event) => startRecording()));
+  elBtnPlay.addEventListener('click', handleErrors((event) => playFromStorage()));
 
   updateUIFromState();
 }
